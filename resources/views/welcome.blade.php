7962475ffa2f744ff216b3439320af32d72699d6
Alvin Yuga Pramana <148262832+bigbosspramana@users.noreply.github.com>
2024-11-22 13:41:16 +0800

    Menambahkan path vite, di welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,24 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" rel="stylesheet">
 
     <!-- Styles / Scripts -->
-    @vite(['resources/js/app.js', 'resources/img/img1.png'])
+    @vite([
+        'resources/js/app.js', 
+        'resources/img/img1.png', 
+        'resources/img/img-self2.jpg',
+        'resources/img/img-self1.jpg',
+        'resources/img/aset1.png',
+        'resources/img/aset2.png',
+        'resources/img/calends.jpg',
+        'resources/img/devforge.jpg',
+        'resources/img/gdsc.jpg',
+        'resources/img/lk21.jpg',
+        'resources/img/oweek.jpg',
+        'resources/img/project1.png',
+        'resources/img/project2.png',
+        'resources/img/project3.png',
+        'resources/img/project4.png',
+        'resources/img/smart-mirror.jpg',
+        'resources/img/topup-shop.jpg',])
 </head>
 
 <body>
