6a504b53e97d4f2ebd18c97ba45784f5fc705558
Alvin Yuga Pramana <148262832+bigbosspramana@users.noreply.github.com>
2024-11-23 00:37:21 +0800

    Memperbaiki file welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <title>Alvin Yuga Pramana Portfolio</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="/build/assets/dashboard-DE21gFif.css" type="text/css">
+    <link rel="stylesheet" href="/build/assets/dashboard-QTQYpU3y.css" type="text/css">
     <link rel="stylesheet" href="/build/assets/app-BaVMVknW.css" type="text/css">
     <link rel="stylesheet" href="/build/assets/reset-8_00JVMR.css" type="text/css">
     <link rel="stylesheet" href="/build/assets/app-DMRfjk3F.css" type="text/css">
@@ -356,7 +356,7 @@
         </div>
     </div>
 
-    <script src="/build/assets/app-DygxLsCf.js"></script>
+    <script src="/build/assets/app-1RNgMjTy.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
